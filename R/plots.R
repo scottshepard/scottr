32a246ffab70c08d0df6bcd48ca426999a87b8c4
Scott Shepard <scott@shepardmail.com>
2018-08-11 21:12:35 -0500

    Add filepath

--- R/plots.R
+++ R/plots.R
@@ -6,20 +6,16 @@
 #' This function will automatically append today's date using the
 #' Sys.Date function to the end of the plot.
 #'
-#' Many of the eSpark scripts are district specific so this
-#' is a way of ensuring that the district name is printed in the plot name
-#' without manually specificying each time.
-#' If no name is given the function will default to Rplot
-#'
 #' @param plot The plot object
 #' @param name Name of the plot to be printed
 #' @param w Width of plot in pixels. Default set to 547
 #' @param h Height of plot in pixels. Default set to 438
+#' @param path Base filepath of the directory to be printed to
 #'
 #' @export
-printPlot <- function(plot, name = "Rplot", w = 547, h = 438) {
+printPlot <- function(plot, name = "Rplot", w = 547, h = 438, path="") {
   name <- stringr::str_replace_all(name, " ", "_")
-  name <- paste0(name, "_", Sys.Date(), ".png")
+  name <- file.path(path, paste0(name, "_", Sys.Date(), ".png"))
   png(name, width=w, height=h)
   print(plot)
   dev.off()
